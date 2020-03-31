1db14955d3331d89e726f6859d13123ebc5a334c
radwiin <38761888+radwiin@users.noreply.github.com>
2020-03-31 22:44:42 +0800

    draggable

---
+++ src/views/draggable/board/components/BoardCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="board-card-wrapper">
+    <div class="board-card">{{ card.content }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoardCard",
+  props: {
+    card: {
+      type: Object
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.board-card-wrapper {
+  margin-bottom: 8px;
+  border-radius: 3px;
+  background-color: #e2e4ea;
+
+  .board-card {
+    background-color: #fff;
+    border-radius: 3px;
+    box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+    cursor: pointer;
+
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+
+    &:hover {
+      background-color: #f4f5f7;
+    }
+  }
+}
+</style>
--- src/views/draggable/board/index.vue
+++ src/views/draggable/board/index.vue
@@ -1,83 +1,23 @@
 <template>
   <div class="page-container">
-    <draggable class="board" v-model="board.columns" v-bind="boardDragOptions">
-      <div
-        class="list-wrapper"
-        v-for="(column, index) in board.columns"
-        :key="index"
-      >
-        <div class="list">
-          <div class="list-header">
-            <div
-              class="list-header-target"
-              v-if="!column.editTitle"
-              @click="onTitleClick($event, column)"
-            ></div>
-            <textarea
-              ref="list-header-name"
-              v-model="column.name"
-              class="list-header-name"
-              spellcheck="false"
-              maxlength="512"
-            />
-          </div>
-          <draggable
-            class="list-content"
-            v-model="column.cards"
-            v-bind="dragOptions"
-          >
-            <div
-              class="list-card-wrapper"
-              v-for="card in column.cards"
-              :key="card.id"
-            >
-              <div class="list-card">{{ card.name }}</div>
-            </div>
-          </draggable>
-          <div class="list-footer">
-            <div v-if="column.add.showInput" class="list-footer-new-container">
-              <div class="list-footer-input-wrapper">
-                <textarea
-                  class="list-footer-input"
-                  v-model="column.add.input"
-                  spellcheck="false"
-                  maxlength="512"
-                />
-              </div>
-              <div class="list-footer-function-container">
-                <input
-                  class="list-footer-new-confirm"
-                  type="submit"
-                  value="添加卡片"
-                />
-                <svg-icon
-                  icon-class="close"
-                  class-name="icon-close"
-                  @click="column.add.showInput = false"
-                />
-              </div>
-            </div>
-            <div
-              v-else
-              class="list-footer-add"
-              @click="column.add.showInput = true"
-            >
-              <svg-icon icon-class="add" class-name="icon-add" />
-              <span>添加卡片</span>
-            </div>
-          </div>
-        </div>
-      </div>
+    <draggable v-model="board.columns" v-bind="boardDragOptions">
+      <board-column
+        v-for="column in board.columns"
+        :key="column.id"
+        :column="column"
+      />
     </draggable>
   </div>
 </template>
 
 <script>
 import draggable from "vuedraggable";
+import BoardColumn from "./components/BoardColumn";
 export default {
   name: "todo-native",
   components: {
-    draggable
+    draggable,
+    BoardColumn
   },
   data() {
     return {
@@ -85,58 +25,49 @@ export default {
         name: "EXIA",
         columns: [
           {
-            name: "TODO",
+            id: "todo",
+            title: "TODO",
             editTitle: false,
             cards: [
-              { name: "A", id: "A" },
-              { name: "B", id: "B" },
-              { name: "C", id: "C" },
-              { name: "D", id: "D" },
-              { name: "E", id: "E" },
-              { name: "F", id: "F" },
-              { name: "G", id: "G" },
-              { name: "H", id: "H" },
-              { name: "I", id: "I" }
-            ],
-            add: {
-              showInput: false,
-              input: ""
-            }
+              { content: "A", id: "A" },
+              { content: "B", id: "B" },
+              { content: "C", id: "C" },
+              { content: "D", id: "D" },
+              { content: "E", id: "E" },
+              { content: "F", id: "F" },
+              { content: "G", id: "G" },
+              { content: "H", id: "H" },
+              { content: "I", id: "I" }
+            ]
           },
           {
-            name: "DOING",
+            id: "doing",
+            title: "DOING",
             cards: [
-              { name: "J", id: "J" },
-              { name: "K", id: "K" },
-              { name: "L", id: "L" },
-              { name: "M", id: "M" },
-              { name: "N", id: "N" },
-              { name: "O", id: "O" },
-              { name: "P", id: "P" },
-              { name: "Q", id: "Q" },
-              { name: "R", id: "R" }
-            ],
-            add: {
-              showInput: false,
-              input: ""
-            }
+              { content: "J", id: "J" },
+              { content: "K", id: "K" },
+              { content: "L", id: "L" },
+              { content: "M", id: "M" },
+              { content: "N", id: "N" },
+              { content: "O", id: "O" },
+              { content: "P", id: "P" },
+              { content: "Q", id: "Q" },
+              { content: "R", id: "R" }
+            ]
           },
           {
-            name: "DONE",
+            id: "done",
+            title: "DONE",
             cards: [
-              { name: "S", id: "S" },
-              { name: "T", id: "T" },
-              { name: "U", id: "U" },
-              { name: "V", id: "V" },
-              { name: "W", id: "W" },
-              { name: "X", id: "X" },
-              { name: "Y", id: "Y" },
-              { name: "Z", id: "Z" }
-            ],
-            add: {
-              showInput: false,
-              input: ""
-            }
+              { content: "S", id: "S" },
+              { content: "T", id: "T" },
+              { content: "U", id: "U" },
+              { content: "V", id: "V" },
+              { content: "W", id: "W" },
+              { content: "X", id: "X" },
+              { content: "Y", id: "Y" },
+              { content: "Z", id: "Z" }
+            ]
           }
         ]
       },
@@ -155,14 +86,6 @@ export default {
         dragClass: "drag"
       }
     };
-  },
-  methods: {
-    onTitleClick(e, column) {
-      // let parentNode = e.target.offsetParent.cloneNode(true);
-      // console.info(parentNode);
-      column.editTitle = true;
-      this.$refs["list-header-name"][0].focus();
-    }
   }
 };
 </script>
@@ -172,220 +95,5 @@ export default {
   padding: 20px;
   overflow-x: auto;
   white-space: nowrap;
-}
-
-.list-wrapper {
-  width: 272px;
-  margin: 0 4px;
-  height: 100%;
-  display: inline-block;
-  white-space: nowrap;
-
-  &:first-child {
-    margin-left: 8px;
-  }
-
-  .list {
-    display: flex;
-    flex-flow: column nowrap;
-    max-height: 100%;
-    border-radius: 3px;
-    white-space: normal;
-    background-color: #ebecf0;
-
-    .list-header {
-      flex: 0 0 auto;
-      padding: 10px 8px;
-      position: relative;
-      min-height: 20px;
-
-      .list-header-target {
-        cursor: pointer;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-      }
-
-      .list-header-name {
-        width: 100%;
-        height: 28px;
-        overflow: hidden;
-        overflow-wrap: break-word;
-        resize: none;
-        background: transparent;
-        border-radius: 3px;
-        box-shadow: none;
-        font-weight: 600;
-        margin: -4px 0;
-        min-height: 20px;
-        max-height: 256px;
-        padding: 4px 8px;
-        border: none;
-        line-height: 20px;
-        font-size: 14px;
-        color: #172b4d;
-        display: block;
-        cursor: pointer;
-
-        &:focus {
-          background: #fff;
-          border: none;
-          box-shadow: inset 0 0 0 2px #0079bf;
-        }
-      }
-    }
-
-    .list-content {
-      flex: 1 1 auto;
-      min-height: 0;
-      overflow-y: auto;
-      overflow-x: hidden;
-      margin: 0 4px;
-      padding: 0 4px;
-
-      .list-card-wrapper {
-        margin-bottom: 8px;
-        border-radius: 3px;
-        background-color: #e2e4ea;
-
-        .list-card {
-          background-color: #fff;
-          border-radius: 3px;
-          box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
-          cursor: pointer;
-
-          height: 60px;
-          text-align: center;
-          line-height: 60px;
-
-          &:hover {
-            background-color: #f4f5f7;
-          }
-        }
-      }
-    }
-
-    .list-footer {
-      flex: 0 0 auto;
-      min-height: 38px;
-
-      .list-footer-add {
-        height: 28px;
-        margin: 2px 8px 8px;
-        padding: 4px 8px;
-
-        display: flex;
-        align-items: center;
-
-        cursor: pointer;
-        color: #5e6c84;
-
-        &:hover {
-          background-color: rgba(9, 30, 66, 0.08);
-          color: #172b4d;
-        }
-
-        .icon-add {
-          width: 20px;
-          height: 20px;
-          margin-right: 2px;
-        }
-
-        span {
-          font-size: 14px;
-          line-height: 20px;
-          font-weight: 400;
-        }
-      }
-
-      .list-footer-new-container {
-        padding-bottom: 8px;
-        margin: 0 8px;
-
-        .list-footer-input-wrapper {
-          padding: 6px 8px 2px;
-          background-color: #fff;
-          border-radius: 3px;
-          box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
-          margin: 0 0 8px;
-
-          .list-footer-input {
-            display: block;
-            width: 100%;
-            height: 54px;
-            overflow: hidden;
-            overflow-wrap: break-word;
-            resize: none;
-            background: transparent;
-            border-radius: 3px;
-            box-shadow: none;
-            min-height: 20px;
-            max-height: 256px;
-            border: none;
-            outline: none;
-            line-height: 20px;
-            font-size: 14px;
-            font-weight: 400;
-            color: #172b4d;
-            padding: 0px;
-            margin-bottom: 4px;
-          }
-        }
-
-        .list-footer-function-container {
-          display: flex;
-          align-items: center;
-
-          .list-footer-new-confirm {
-            background-color: #5aac44;
-            box-shadow: none;
-            border: none;
-            color: #fff;
-            cursor: pointer;
-            display: inline-block;
-            font-size: 14px;
-            font-weight: 400;
-            line-height: 20px;
-            margin: 0 4px 0 0;
-            padding: 6px 12px;
-            text-align: center;
-            border-radius: 3px;
-
-            &:hover {
-              background-color: #61bd4f;
-              box-shadow: none;
-              border: none;
-              color: #fff;
-            }
-          }
-
-          .icon-close {
-            width: 20px;
-            height: 20px;
-            margin-left: 6px;
-            cursor: pointer;
-            color: #6b778c;
-
-            &:hover {
-              color: #172b4d;
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-.ghost {
-  & > .list-card {
-    opacity: 0;
-  }
-}
-
-.drag {
-  opacity: 1 !important;
-  transform: rotate(7deg);
 }
 </style>
