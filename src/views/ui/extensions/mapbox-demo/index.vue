6f379ef0bd7970835af47c6aa453a5945dd689eb
radwiin <radwiinxu@outlook.com>
2020-08-19 15:27:02 +0800

    1

--- src/views/ui/extensions/mapbox-demo/index.vue
+++ src/views/ui/extensions/mapbox-demo/index.vue
@@ -14,11 +14,10 @@ export default {
     mapboxgl.accessToken = 'pk.eyJ1IjoicmFkd2lpbiIsImEiOiJja2R5Nmh1MWwxY3J0Mndub285dHIwc3A5In0.ac5hyVhwTJacJf92xeYRTg'
     const map = new mapboxgl.Map({
       container: this.$refs.mapbox,
-      style: 'mapbox://styles/mapbox/streets-v10', // stylesheet location
-      center: [114, 38.54], // starting position [lng, lat]
+      style: 'mapbox://styles/radwiin/ckdy713s545f919p7x8ea5pt3', // stylesheet location
+      center: [120.52139282226561, 32.56041416198585], // starting position [lng, lat]
       zoom: 9 // starting zoom
     })
-    map.addControl(new MapboxLanguage({ defaultLanguage: 'zh' }))
   }
 }
 </script>
